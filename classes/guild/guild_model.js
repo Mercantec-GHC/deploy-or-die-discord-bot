ff52c1787e72aa806d0a3caa732bf3da3b1e8e45
René <reneskjodt333@gmail.com>
2026-06-09 15:05:47 +0200

    Update interface validation in Guild class constructor to use isSpecificInterface method

--- classes/guild/guild_model.js
+++ classes/guild/guild_model.js
@@ -3,7 +3,7 @@ import GuildCreateInterface from "./guild_create_interface.js";
 export default class Guild extends Model {
     constructor(guildCreateInterface) {
 
-        if (!GuildCreateInterface.isInterface(guildCreateInterface)) {
+        if (!GuildCreateInterface.isSpecificInterface(guildCreateInterface)) {
             throw new Error("Invalid interface");
         }
 
