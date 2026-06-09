da0db1179b6a9e5c35bb7b2e90f1d631a40ff191
René <reneskjodt333@gmail.com>
2026-06-09 14:30:09 +0200

    Add Interface class and update index.js to utilize it; enable debugging in package.json

---
+++ classes/abstract_classes/interface.js
@@ -0,0 +1,5 @@
+export default class Interface {
+    constructor(args) {
+        Object.assign(this, args);
+    }
+}
--- index.js
+++ index.js
@@ -1,2 +1,8 @@
+import Interface from "./classes/abstract_classes/interface.js";
+
+const inter = new Interface({"test": "test"});
+
+console.log(inter);
 console.log("Hello world!");
+
 console.log("test");
