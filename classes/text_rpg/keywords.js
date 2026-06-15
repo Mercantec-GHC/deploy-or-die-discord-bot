5f978d269f5392d4c994e17c0dc884078d8034c7
Budget Knight <mads.bargsteen@live.dk>
2026-06-15 14:25:04 +0200

    Normalize keywords to lowercase and refactor encounter retrieval logic

--- classes/text_rpg/keywords.js
+++ classes/text_rpg/keywords.js
@@ -2,18 +2,18 @@ import EncounterKeywords from "./encounter_keywords.js";
 export default class Keywords {
 
     constructor(message){
-        this.keywords = message.trim().split(" ").filter((k)=>k.length > 0);
+        this.keywords = message.trim().toLowerCase().split(" ").filter((k)=>k.length > 0);
 
 
 
     }
     get encounter(){
-        EncounterKeywords
-        .forEach((k)=>{
+        return EncounterKeywords 
+        .filter((k)=>{
             if(this.keywords.sort((a, b) => Math.random() - 0.5).includes(k)){
                 return k;
             }
-        });
+        })[0];
         return null;
     }
 
