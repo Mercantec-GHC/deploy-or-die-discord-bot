b2cf0400bc59745e9066a52b24f308445a56c565
Kasper Lundby <venoelundby@gmail.com>
2026-06-15 15:29:38 +0200

    pity timer

--- classes/text_rpg/Enemies/Docker.js
+++ classes/text_rpg/Enemies/Docker.js
@@ -1,3 +1,5 @@
+import Enemy from "./enemy.js"
+
 export default class Docker extends Enemy {
     constructor() {
         super("Docker", 100);
--- classes/text_rpg/encounter.js
+++ classes/text_rpg/encounter.js
@@ -1,8 +1,34 @@
+import Docker from "./Enemies/Docker.js"
+
 export default class Encounter {
-        static encounter_chance = 20; // percentage
+
+        static encounter_chance = 10; // percentage
+        static max_missed_encounters = 2
+        static missed_encounters = 0
     constructor(keyword){
         this.keyword = keyword;
-        this.is_encountered = Math.random() * 100 < Encounter.encounter_chance;
+        this.is_encountered = Encounter.calculate_chance();
+        this.enemy = Encounter.encounter_enemy(keyword);
+        
+    }
+    static calculate_chance() {
+        let is_encountered = Math.random() * 100 < this.encounter_chance;
+        if (!is_encountered) {
+            this.missed_encounters++;
+            if (this.missed_encounters >= this.max_missed_encounters) {
+                this.missed_encounters = 0;
+                return true
+            }
+        }
+        else {
+            this.missed_encounters = 0;
+        }
+        return is_encountered;
     }
 
+    static encounter_enemy(keyword) {
+        return {
+            "docker": Docker
+        }[keyword]
+    }
 }
