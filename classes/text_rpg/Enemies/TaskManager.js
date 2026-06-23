2889cf112464bfc5533cf01a8ef791e3ac89cdbc
René <reneskjodt333@gmail.com>
2026-06-23 11:07:48 +0200

    Format constructor parameters in TaskManager class for improved readability

--- classes/text_rpg/Enemies/TaskManager.js
+++ classes/text_rpg/Enemies/TaskManager.js
@@ -2,6 +2,12 @@ import Enemy from "./enemy.js";
 
 export default class TaskManager extends Enemy {
     constructor(encounter) {
-        super("Unresponsive Task Manager", 500, 50, encounter, "on the wall before you is craved strange symbols and diagrams, as you attempt to decipher them a sharp pain shoots through your head, a cold voice whispers in your mind overwhelming you senses and thoughts with the sound of static. roll for initiative!")
+        super(
+            "Unresponsive Task Manager", // Name
+            500, // HP
+            50, // Attack
+            encounter, // Encounter
+            "on the wall before you is craved strange symbols and diagrams, as you attempt to decipher them a sharp pain shoots through your head, a cold voice whispers in your mind overwhelming you senses and thoughts with the sound of static. roll for initiative!" // Description
+        )
     }
 }
