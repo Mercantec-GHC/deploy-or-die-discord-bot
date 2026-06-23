5af0462d987b7f5e27d75a8c928c3f93f9e55358
René <reneskjodt333@gmail.com>
2026-06-23 11:07:41 +0200

    Format constructor parameters in IpMacAddress class for improved readability

--- classes/text_rpg/Enemies/IpMacAddress.js
+++ classes/text_rpg/Enemies/IpMacAddress.js
@@ -2,6 +2,12 @@ import Enemy from "./enemy.js";
 
 export default class IpMacAddress extends Enemy {
     constructor(encounter) {
-        super("Ip and Mac Ad Ress", 500, 50, encounter, "The Twinheaded messenger Ip and Mac Ad Ress stands before you a terrifying amalgamation of static and shifting construction, in this case shooting the messenger is recommended. roll for initiative!")
+        super(
+            "Ip and Mac Ad Ress", // Name
+            500, // HP
+            50, // Attack
+            encounter, // Encounter
+            "The Twinheaded messenger Ip and Mac Ad Ress stands before you a terrifying amalgamation of static and shifting construction, in this case shooting the messenger is recommended. roll for initiative!" // Description
+        )
     }
 }
